2a21fb21bc2d6d1503cb7d1da2dc512f16f43fdb
pikachungg <34949943+pikachungg@users.noreply.github.com>
2020-02-08 14:50:44 -0500

    maybeee

--- temp_script.js
+++ temp_script.js
@@ -3,5 +3,26 @@ function copyClipBoard(){
     copyText.select()
     document.execCommand("copy");
     alert("Copied!");
-    console.log("XDDDDDDD");
 }
+
+function fetchMeDaddy(){
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+    } else { // code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+      if (this.readyState==4 && this.status==200) {
+        var result = this.responseText;
+        //document.getElementById("info").innerHTML=result; //Prints raw data
+        var r = JSON.parse(this.responseText);
+        console.log(r);
+      }
+    }
+      xmlhttp.open("GET", "", true);
+      xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xmlhttp.send();
+  }
+
+  
